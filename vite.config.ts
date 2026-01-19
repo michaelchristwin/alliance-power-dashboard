524a58913c9068b26071f886cce8c948ea9dfea8
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-01-20 00:40:58 +0100

    Chore: Added nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -7,7 +8,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), react(), tailwindcss()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
