e7efb3aec4bd9a10eee7e22e7cdb7a1d332b021d
michaelchristwin <kelechukwuchristwin@gmail.com>
2026-01-23 21:09:49 +0100

    Chore: Fixed vite issues

---
+++ src/components/ui/functions/sidebarContext.tsx
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+import type { SidebarContextProps } from "../sidebar";
+
+export const SidebarContext = createContext<SidebarContextProps | null>(null);
+
+export function useSidebar() {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.");
+  }
+
+  return context;
+}
